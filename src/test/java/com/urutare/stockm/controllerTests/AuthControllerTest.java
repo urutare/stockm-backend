6595de3a81aaafa03e9224d078b5237a1e8e1400
Celestin Niyindagiriye <nicelestin08@gmail.com>
2023-04-28 15:35:43 +0200

    Revert Auth test to integration test

--- src/test/java/com/urutare/stockm/controllerTests/AuthControllerTest.java
+++ src/test/java/com/urutare/stockm/controllerTests/AuthControllerTest.java
@@ -1,60 +1,85 @@
 package com.urutare.stockm.controllerTests;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.urutare.stockm.controller.AuthController;
 import com.urutare.stockm.entity.User;
 import com.urutare.stockm.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(AuthController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @LocalServerPort
+    private int port;
 
-    @MockBean
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
     private UserService userService;
 
     @Test
-    public void testLogin() throws Exception {
-        // create a mock UserMap for the request body
-        Map<String, Object> UserMap = new HashMap<>();
-        UserMap.put("email", "test@example.com");
-        UserMap.put("password", "testpassword");
+    public void testSignup() throws Exception {
+        // create a mock userMap for the request body
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("email", "test@example.com");
+        userMap.put("password", "testpassword");
 
         // create a mock User object for the response
         User user = new User();
         user.setId(1L);
         user.setEmail("test@example.com");
-        user.setPassword("testpassword");
+        user.setFullName("Test User");
 
-        // mock the userService method to return the mock User object
-        when(userService.validateUser(anyString(), anyString())).thenReturn(user);
+        userService.registerUser(user);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         // send the request and verify the response
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(UserMap)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token", notNullValue()));
+        String url = "http://localhost:" + port + "/api/auth/signup";
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(userMap, headers);
+
+        Map<String, Object> response = restTemplate.exchange(url, HttpMethod.POST, request, Map.class).getBody();
+        System.out.println(response.toString());
+        assertThat(response, notNullValue());
+        assertThat(response.get("id"), notNullValue());
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        // create a mock userMap for the request body
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("email", "test@example.com");
+        userMap.put("password", "testpassword");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(userMap, headers);
+
+        // send the request and verify the response
+        String url = "http://localhost:" + port + "/api/auth/login";
+        Map<String, Object> response = restTemplate.exchange(url, HttpMethod.POST, request, Map.class).getBody();
+        System.out.println(response.toString());
+
+        assertThat(response, notNullValue());
+        assertThat(response.get("token"), notNullValue());
     }
 }
