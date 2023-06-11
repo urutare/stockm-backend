f829f4d429e82126a005f4c4b6c783dee1c04fa9
Celestin Niyindagiriye <nicelestin08@gmail.com>
2023-06-11 23:52:30 +0200

    chore: add stock service

---
+++ stock-service/src/main/java/com/urutare/stockservice/constants/WhiteList.java
@@ -0,0 +1,11 @@
+package com.urutare.stockservice.constants;
+
+public class WhiteList {
+
+    public static final String[] WHITELIST_URLS = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-ui.html"
+    };
+
+}
