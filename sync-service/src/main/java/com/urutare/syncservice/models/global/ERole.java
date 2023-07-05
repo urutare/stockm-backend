c84470688fad681e00c2ad775ca2dc9170979855
Celestin Niyindagiriye <nicelestin08@gmail.com>
2023-07-05 18:58:23 +0200

    feat: add security

---
+++ sync-service/src/main/java/com/urutare/syncservice/models/global/ERole.java
@@ -0,0 +1,5 @@
+package com.urutare.syncservice.models.global;
+
+public enum ERole {
+    ADMIN, SELLER, BUYER, MANAGER, USER;
+}
---
+++ sync-service/src/main/java/com/urutare/syncservice/models/global/TokenType.java
@@ -0,0 +1,9 @@
+package com.urutare.syncservice.models.global;
+
+public enum TokenType {
+    ACCESS_TOKEN,
+    REFRESH_TOKEN,
+    RESET_PASSWORD_TOKEN,
+    VERIFY_EMAIL_TOKEN,
+    VERIFY_PHONE_TOKEN;
+}
