35ff50a2390135f0f4e12ef1ee3a4fa5fafbe70f
Celestin Niyindagiriye <nicelestin08@gmail.com>
2023-08-06 16:17:01 +0200

    feat: fix graphql

--- stock-service/src/main/java/com/urutare/stockservice/controller/ProductSoldController.java
+++ stock-service/src/main/java/com/urutare/stockservice/controller/ProductSoldController.java
@@ -1,7 +1,10 @@
 package com.urutare.stockservice.controller;
 
+import com.urutare.stockservice.aspect.RequiresRole;
 import com.urutare.stockservice.entities.ProductSold;
+import com.urutare.stockservice.models.enums.UserRole;
 import com.urutare.stockservice.repository.ProductSoldRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -13,9 +16,13 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ProductSoldController {
     private final ProductSoldRepository productSoldRepository;
+    private final HttpServletRequest request;
 
     @QueryMapping
+    @RequiresRole(UserRole.ADMIN)
     public ProductSold productSoldById(@Argument UUID id) {
+        UUID userId = UUID.fromString(request.getHeader("userId"));
+        System.out.println("userId = " + userId);
         return productSoldRepository.findById(id).orElse(null);
     }
 
