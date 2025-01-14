d101b5f60877f9afb7f1d0d13ad67fed683bb62c
Celestin Niyindagiriye <nicelestin08@gmail.com>
2025-01-14 10:21:22 +0200

    Upgrade java to 21 version

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Array of repository names
 # shellcheck disable=SC2054
-repositories=("stockm-config-server" "stockm-category-service" "stockm-sync-service" "stockm-stock-service" "stockm-discovery-service" "stockm-auth-service" "stockm-api-gateway" "stockm-payment-service" "stockm-common-core" "sms-gateway")
+repositories=("stockm-config-server" "stockm-category-service" "stockm-sync-service" "stockm-stock-service" "stockm-discovery-service" "stockm-auth-service" "stockm-api-gateway" "stockm-payment-service" "stockm-common-core" "sms-gateway" "POSUtils" "POSFunctional")
 # Base URL for the GitHub organization
 base_url="https://github.com/urutare/"
 # Loop through the repositories array
